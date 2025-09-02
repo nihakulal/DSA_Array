e24487734e700418f0244ae2539a7850f17bb582
nihakulal <nihakulal159@gmail.com>
2025-09-02 20:58:23 +0530

    fibonacci.java

---
+++ fibonacci.java
@@ -0,0 +1,14 @@
+public class Test3 {
+    public static void main(String[] args) {
+        int n = 13;
+        int a=0,b=1;
+        while(a<=n){
+            System.out.print(a+" ");
+            int c=a+b;
+            a=b;
+            b=c;
+            
+
+        }
+    }
+}
